4ce69d088d86ca90713d473c983fc3065b1a008d
Jae-Won Jang <jw-jang@outlook.com>
2026-02-03 23:32:07 -0500

    Modified until the error

--- ch2-guessing-game/src/main.rs
+++ ch2-guessing-game/src/main.rs
@@ -1,7 +1,26 @@
+// use std::io;
+
+// fn main() {
+//   println!("guess the number!");
+//   println!("Please input your guess.");
+//   let mut guess = String::new();
+
+//   io::stdin()
+//     .read_line(&mut guess)
+//     .expect("Failed to read line");
+
+//   println!("You guessed: {guess}");
+// }
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
   println!("guess the number!");
+  let secret_number = rand::thread_rng().gen_range(1..=100);
+  println!("The secret number is: {secret_number}");
+
   println!("Please input your guess.");
   let mut guess = String::new();
 
@@ -10,4 +29,10 @@ fn main() {
     .expect("Failed to read line");
 
   println!("You guessed: {guess}");
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Equal => println!("You win!"),
+    Ordering::Greater => println!("Too big!"),
+  }
 }
